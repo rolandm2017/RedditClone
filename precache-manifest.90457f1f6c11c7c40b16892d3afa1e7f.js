e722a929644422b219cf54b805618f0495a54c72
plutownium <rolandmackintosh@gmail.com>
2020-01-07 23:23:10 -0800

    Updates

---
+++ precache-manifest.90457f1f6c11c7c40b16892d3afa1e7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8125dba8c0c67e23111802e964c21e",
+    "url": "/RedditClone/index.html"
+  },
+  {
+    "revision": "b767e97925880c2ed8e6",
+    "url": "/RedditClone/static/css/main.db32f98f.chunk.css"
+  },
+  {
+    "revision": "7fad9d15d91f5591cf4e",
+    "url": "/RedditClone/static/js/2.49554939.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/RedditClone/static/js/2.49554939.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b767e97925880c2ed8e6",
+    "url": "/RedditClone/static/js/main.c528a1b6.chunk.js"
+  },
+  {
+    "revision": "4d5ae6c45e58ae376356",
+    "url": "/RedditClone/static/js/runtime-main.3461f356.js"
+  }
+]);
