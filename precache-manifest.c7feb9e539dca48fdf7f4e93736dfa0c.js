2a35e19755c5f4dbf72a5478a2b8fbc830223d24
plutownium <rolandmackintosh@gmail.com>
2020-01-10 13:40:39 -0800

    Updates

---
+++ precache-manifest.c7feb9e539dca48fdf7f4e93736dfa0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8fb899205d4d1496464853d83da73c",
+    "url": "/RedditClone2/index.html"
+  },
+  {
+    "revision": "42caf212a6d18f191358",
+    "url": "/RedditClone2/static/css/main.59ae884f.chunk.css"
+  },
+  {
+    "revision": "2d490719687885f02f01",
+    "url": "/RedditClone2/static/js/2.7233314d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/RedditClone2/static/js/2.7233314d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "42caf212a6d18f191358",
+    "url": "/RedditClone2/static/js/main.7a67cb92.chunk.js"
+  },
+  {
+    "revision": "d05c90c1525b6af88fdc",
+    "url": "/RedditClone2/static/js/runtime-main.298b1c65.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RedditClone/precache-manifest.54e736718fb116f8956b0b3724e0066c.js"
+  "/RedditClone2/precache-manifest.c7feb9e539dca48fdf7f4e93736dfa0c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RedditClone/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RedditClone2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
